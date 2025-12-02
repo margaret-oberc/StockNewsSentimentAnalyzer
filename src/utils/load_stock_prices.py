8cf0d701c2ff3ca771ba970884ff423b2cb86a87
margaret <marga@LAPTOP-6A168KH4>
2025-12-02 17:17:12 -0500

    Added more stocks

--- src/utils/load_stock_prices.py
+++ src/utils/load_stock_prices.py
@@ -103,7 +103,7 @@ def store_fundamentals_to_db(connection, symbol, trading_date):
 def main():
     # List of stock tickers to analyze
     tickers = ['^GSPTSE', 'ACO-X.TO', 'AEM.TO', 'ALA.TO', 'ALC.TO', 'AQN', 'BCE', 'BMO', 'BNS', 'CM', 'CP', 'CU.TO', 'EMA.TO', 'ENB', 'FC.TO', 'FTS', 'GWO.TO', 'H.TO', 'IFC.TO', 'KEY.TO', 'L.TO', 'MFC',
-                'MKP.TO', 'MRU.TO', 'NA.TO', 'PAAS', 'POW.TO', 'PPL.TO', 'PXT.TO', 'QBR-B.TO', 'RCI-B.TO', 'RSI.TO', 'RY', 'SIA.TO', 'SLF-PC.TO', 'SU', 'T.TO', 'TD', 'TRI', 'USA.TO', 'WCN.TO', 'WN.TO', 'X.TO']
+                'MKP.TO', 'MRU.TO', 'NA.TO', 'PAAS', 'POW.TO', 'PPL.TO', 'PXT.TO', 'QBR-B.TO', 'RCI-B.TO', 'RSI.TO', 'RY', 'SIA.TO', 'SLF.TO', 'SU', 'T.TO', 'TD', 'TRI', 'USA.TO', 'WCN.TO', 'WN.TO', 'X.TO']
     start_dates = ['2017-01-01', '2018-01-01', '2019-01-01', '2020-01-01', '2021-01-01', '2022-01-01', '2023-01-01', '2024-01-01', '2025-01-01']
     
     try:
