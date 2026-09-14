48815d87008793b78c3d28faac71b5bc054955cc
margaret <marga@LAPTOP-6A168KH4>
2026-09-14 12:10:39 -0400

    Changed stock list

--- src/stock_news_sentiment_analyzer.py
+++ src/stock_news_sentiment_analyzer.py
@@ -12,8 +12,8 @@ from typing import Optional
 from pathlib import Path
 
 # List of stock tickers to analyze
-tickers = ['ACO-X.TO', 'AEM.TO', 'ALA.TO', 'ALC.TO', 'ALS.TO', 'ARE.TO', 'AQN', 'ATD.TO', 'AW.TO', 'BCE', 'BDGI.TO', 'BMO', 'BN', 'BNS', 'CCO.TO', 'CEU.TO', 'CM',
-                'CNQ.TO', 'CNR.TO', 'CPX.TO', 'CVE.TO', 'CVG.TO',  'CP', 'CU.TO', 'EIF.TO', 'EMA.TO', 'ENB', 'FC.TO', 'FFH.TO', 'FTS', 'GEI.TO', 'GWO.TO', 'H.TO', 'HPS-A.TO', 'IMO.TO', 'IFC.TO', 'KEY.TO', 'L.TO',
+tickers = ['ACO-X.TO', 'AEM.TO', 'ALA.TO', 'ALC.TO', 'ALS.TO', 'ARE.TO', 'AQN', 'ATD.TO', 'AW.TO', 'BCE', 'BDGI.TO', 'BIP-UN.TO', 'BMO', 'BN', 'BNS', 'CCO.TO', 'CEU.TO', 'CM',
+                'CNQ.TO', 'CNR.TO', 'CPX.TO', 'CVE.TO', 'CVG.TO', 'CP', 'CU.TO', 'EIF.TO', 'EMA.TO', 'ENB', 'FC.TO', 'FFH.TO', 'FRU.TO', 'FTS', 'GEI.TO', 'GWO.TO', 'H.TO', 'HPS-A.TO', 'IMO.TO', 'IFC.TO', 'KEY.TO', 'L.TO',
                 'MEQ.TO', 'MFC', 'MKP.TO', 'MRU.TO', 'NA.TO', 'PAAS', 'POW.TO', 'PPL.TO', 'PXT.TO', 'QBR-B.TO', 'RCI-B.TO', 'RSI.TO', 'RY',
                 'SIA.TO', 'SLF.TO', 'SOBO.TO', 'SU', 'T.TO', 'TIH.TO', 'TD', 'TRI', 'TRP.TO', 'WCN.TO', 'WN.TO', 'WPM.TO', 'X.TO']
 
--- src/utils/load_stock_prices.py
+++ src/utils/load_stock_prices.py
@@ -103,8 +103,8 @@ def store_fundamentals_to_db(connection, symbol, trading_date):
 def main():
     # List of stock tickers to analyze
     tickers = ['^GSPTSE', 
-                'ACO-X.TO', 'AEM.TO', 'ALA.TO', 'ALC.TO', 'ALS.TO', 'ARE.TO', 'AQN', 'ATD.TO', 'AW.TO', 'BCE', 'BDGI.TO', 'BMO', 'BN', 'BNS', 'CCO.TO', 'CEU.TO', 'CM',
-                'CNQ.TO', 'CNR.TO', 'CPX.TO', 'CVE.TO', 'CVG.TO',  'CP', 'CU.TO', 'EIF.TO', 'EMA.TO', 'ENB', 'FC.TO', 'FFH.TO', 'FTS', 'GWO.TO', 'GEI.TO', 'H.TO', 'HPS-A.TO', 'IMO.TO', 'IFC.TO', 'KEY.TO', 'L.TO',
+                'ACO-X.TO', 'AEM.TO', 'ALA.TO', 'ALC.TO', 'ALS.TO', 'ARE.TO', 'AQN', 'ATD.TO', 'AW.TO', 'BCE', 'BDGI.TO', 'BIP-UN.TO', 'BMO', 'BN', 'BNS', 'CCO.TO', 'CEU.TO', 'CM',
+                'CNQ.TO', 'CNR.TO', 'CPX.TO', 'CVE.TO', 'CVG.TO',  'CP', 'CU.TO', 'EIF.TO', 'EMA.TO', 'ENB', 'FC.TO', 'FFH.TO', 'FRU.TO', 'FTS', 'GWO.TO', 'GEI.TO', 'H.TO', 'HPS-A.TO', 'IMO.TO', 'IFC.TO', 'KEY.TO', 'L.TO',
                 'MEQ.TO', 'MFC', 'MKP.TO', 'MRU.TO', 'NA.TO', 'PAAS', 'POW.TO', 'PPL.TO', 'PXT.TO', 'QBR-B.TO', 'RCI-B.TO', 'RSI.TO', 'RY',
                 'SIA.TO', 'SLF.TO', 'SOBO.TO', 'SU', 'T.TO', 'TIH.TO', 'TD', 'TRI', 'TRP.TO', 'WCN.TO', 'WN.TO', 'WPM.TO', 'X.TO']
     start_dates = ['2017-01-01', '2018-01-01', '2019-01-01', '2020-01-01', '2021-01-01', '2022-01-01', '2023-01-01', '2024-01-01', '2025-01-01', '2026-01-01']
